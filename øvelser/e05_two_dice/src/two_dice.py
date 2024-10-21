8b321a9e33521dc1acfc04e6f522643c58d4856c
Tobiascbes <Tobias.besser2300@gmail.com>
2024-10-21 12:40:17 +0200

    E01 - E05

--- øvelser/e05_two_dice/src/two_dice.py
+++ øvelser/e05_two_dice/src/two_dice.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 def main():
-    pass
+    diceOne = (1, 2, 3, 4, 5, 6)
+    diceTwo = (1, 2, 3, 4, 5, 6)
+    for i in diceOne:
+        for j in diceTwo:
+            if i + j ==5:
+                print(f"({i},{j})")
 
 if __name__ == "__main__":
     main()
